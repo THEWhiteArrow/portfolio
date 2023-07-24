b743091c60f9a181d78aeaedd047a144a12c3a1c
Damian Trafialek <damian.trafialk@gmail.com>
2023-07-25 00:30:50 +0200

    bug: project tile shadow not working

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -9,16 +9,11 @@ export default function Project(props: Props) {
     const { title, description, imgName, imgAlt, shadowColor, shorten } = props
 
     const image = require(`../assets/image/${imgName}`)
-    // let hider = null
-    // if (!display)
-    //     hider = (
-    //         <div className="absolute w-full h-full top-0 left-0 fadeLikeMedium"></div>
-    //     )
 
     return (
         <div className={`Project p-5 min-w-10 max-w-25 relative`}>
-            {/* {hider} */}
-            <div className={`Project-img-container  w-full my-5 shadow-md shadow-[${shadowColor}] `}  >
+            <div className={`Project-img-container  w-full my-5 shadow-[${'red'}] shadow-md `}  >
+                {/* <div className={`Project-img-container  w-full my-5 shadow-md shadow-[${shadowColor}] `}  > */}
                 <img src={image} alt={imgAlt} className="w-full h-48 object-cover" />
             </div>
             <h3 className="Project-title text-3xl text-center px-2">{title}</h3>
