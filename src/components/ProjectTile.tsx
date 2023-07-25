fa33b82af7ae2aa0965f799d5073442046c02fb4
Damian Trafialek <damian.trafialk@gmail.com>
2023-07-25 18:16:14 +0200

    fixed projects shadow

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -12,9 +12,7 @@ export default function Project(props: Props) {
 
     return (
         <div className={`Project p-5 min-w-10 max-w-25 relative`}>
-            {/* strange bug while development - to have shadowColors working you need to run those colors as strings and the put variable in their name */}
-            <div className={`Project-img-container  w-full my-5 shadow-[${shadowColor}] shadow-md `}  >
-                {/* <div className={`Project-img-container  w-full my-5 shadow-md shadow-[${shadowColor}] `}  > */}
+            <div className='Project-img-container  w-full my-5' style={{ boxShadow: '0px 5px 10px 2px ' + shadowColor }}>
                 <img src={image} alt={imgAlt} className="w-full h-48 object-cover" />
             </div>
             <h3 className="Project-title text-3xl text-center px-2">{title}</h3>
