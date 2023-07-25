7ae0215cb914d2e0ff83a635db67a40dc06f90f1
Damian Trafialek <damian.trafialk@gmail.com>
2023-07-26 01:36:59 +0200

    add form validation

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,17 +10,74 @@ import { ReactComponent as InstagramSvg } from '../assets/svg/instagram.svg'
 import { Link } from 'react-router-dom'
 
 
-export default class Contact extends Component {
+
+export default class Contact extends Component<any, any> {
     constructor(props: any) {
         super(props)
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+            nameValid: false,
+            emailValid: false,
+            messageValid: false,
+            isValidating: false
+        }
+    }
+
+    required = (value: string) => new RegExp('^[\\w ]+$').test(value.trim())
+    email = (value: string) => new RegExp('^\\w(\\w*|\\.[^.]+)*@[\\w]+\\.[a-zA-Z]{2,}$').test(value)
+
+    handleSubmit = (e: any) => {
+        e.preventDefault()
+        this.setState({ isValidating: true })
+        const { name, email, message } = this.state
+
+        const nameValid = this.required(name)
+        const emailValid = this.email(email)
+        const messageValid = this.required(message)
+
+        console.log(nameValid, emailValid, messageValid)
+
+        this.setState({ isValidating: true, nameValid, emailValid, messageValid })
+        if (nameValid && emailValid && messageValid) {
+            console.log('success')
+        }
+        else {
+            console.log('fail')
+        }
+    }
+
+    handleChangeCupture = (e: any) => {
+        this.setState(() => ({ [e.target.name]: e.target.value }),
+            () => {
+                if (this.state.isValidating) {
+                    const { name, email, message } = this.state
+
+                    const nameValid = this.required(name)
+                    const emailValid = this.email(email)
+                    const messageValid = this.required(message)
+
+                    const st = { nameValid, emailValid, messageValid }
+                    this.setState(st)
+                }
+            })
+
     }
 
 
     render() {
+        let nameFieldInfoStyle = ''
+        let emailFieldInfoStyle = ''
+        let messageFieldInfoStyle = ''
 
-
+        if (this.state.isValidating) {
+            nameFieldInfoStyle = this.state.nameValid ? 'valid-name' : 'invalid-name'
+            emailFieldInfoStyle = this.state.emailValid ? 'valid-email' : 'invalid-email'
+            messageFieldInfoStyle = this.state.messageValid ? 'valid-message' : 'invalid-message'
+        }
         return (
-            <section id="contact" className="Contact flex bg-black text-white">
+            <section id="contact" className="Contact flex bg-black text-white" >
                 <div className="container py-8 relative grow flex flex-col ">
                     <Title content='Contact' colorType='white' />
 
@@ -39,30 +96,30 @@ export default class Contact extends Component {
                         </div>
 
 
-                        <form className="py-10 shadow-md rounded grow flex flex-col justify-center md:pl-10">
+                        <form onSubmitCapture={this.handleSubmit} className="py-10 shadow-md rounded grow flex flex-col justify-center md:pl-10">
                             <p>Or email me directly on <Link to="mailto:damian.trafialek@gmail.com" className='underline'>damian.trafialek@gmail.com</Link></p>
-                            <div className="mt-5 mb-4">
+                            <div className={`mt-10 ${nameFieldInfoStyle}`}>
                                 <label className="block mb-2" htmlFor="name">Name</label>
-                                <input
+                                <input value={this.state.name} onChangeCapture={this.handleChangeCupture}
                                     className="appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-white focus:shadow-outline"
-                                    id="name" type="text" placeholder="Your precious name" />
+                                    id="name" name="name" type="text" placeholder="Your precious name" />
                             </div>
-                            <div className="mb-4">
+                            <div className={`mt-10 ${emailFieldInfoStyle}`} >
                                 <label className="block mb-2" htmlFor="email">Email</label>
-                                <input
+                                <input value={this.state.email} onChangeCapture={this.handleChangeCupture}
                                     className="appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-white focus:shadow-outline"
-                                    id="email" type="email" placeholder="Your communication email" />
+                                    id="email" name="email" type="email" placeholder="Your communication email" />
                             </div>
-                            <div className="mb-6">
+                            <div className={`mt-10 ${messageFieldInfoStyle}`}>
                                 <label className="block mb-2" htmlFor="message">Message</label>
-                                <textarea
+                                <textarea value={this.state.message} onChangeCapture={this.handleChangeCupture}
                                     className="appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-white focus:shadow-outline"
-                                    id="message" rows={4} placeholder="Your thoughts"></textarea>
+                                    id="message" name="message" rows={4} placeholder="Your thoughts"></textarea>
                             </div>
-                            <div className="flex items-center justify-center">
+                            <div className="mt-10 flex items-center justify-center">
                                 <button
                                     className="w-full p-2 rounded-md outline outline-white hover:bg-white hover:text-black hover:shadow-xl transition duration-200 ease-in-out"
-                                    type="button">
+                                    type="submit">
                                     Punch It
                                 </button>
                             </div>
@@ -105,3 +162,4 @@ export default class Contact extends Component {
     </div>
 </div> 
 */}
+
