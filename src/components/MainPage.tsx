913400d934433d00d83571ad56daa0f622badd2f
Damian Trafialek <damian.trafialk@gmail.com>
2023-07-30 00:11:01 +0200

    add: initial elmet descriptions

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,31 +1,41 @@
-import React, { Component } from 'react'
-import About from './About'
-import Contact from './Contact'
-import Hero from './Hero'
-import Projects from './Projects'
-import Skills from './Skills'
-import Vision from './Vision' 
-import { GlobalState } from '../App'
+import React, { Component } from "react";
+import About from "./About";
+import Contact from "./Contact";
+import Hero from "./Hero";
+import Projects from "./Projects";
+import Skills from "./Skills";
+import Vision from "./Vision";
+import { GlobalState } from "../App";
 
-import Aos from 'aos'
-
+import Aos from "aos";
+import { Helmet } from "react-helmet";
 class MainPage extends Component<GlobalState> {
-    
-    componentDidMount(): void {
-        Aos.init()
-    }
-    render() {
-        return (
-            <>
-                <Hero />
-                <Projects windowWidth={this.props.windowWidth} />
-                <About />
-                <Skills />
-                <Vision />
-                <Contact />
-            </>
-        )
-    }
+  componentDidMount(): void {
+    Aos.init();
+  }
+  render() {
+    return (
+      <>
+        <Helmet>
+          <title>Damian Trafiałek | Portfolio</title>
+          <meta
+            name="description"
+            content="Step into Damian Trafiałek's World - a 20-year-old Technology Engineering Student hailing from Denmark. Unveil a realm of innovation, creativity, and tech expertise through captivating projects and skill showcases. Join me on this exciting journey of problem-solving and discovery. Let's connect and shape the future together!"
+          />
+          <meta
+            name="keywords"
+            content="portfolio damian trafialek trafiałek projects experience skills high professional"
+          />
+        </Helmet>
+        <Hero />
+        <Projects windowWidth={this.props.windowWidth} />
+        <About />
+        <Skills />
+        <Vision />
+        <Contact />
+      </>
+    );
+  }
 }
 
-export default MainPage
+export default MainPage;
--- src/components/MaintanancePage.tsx
+++ src/components/MaintanancePage.tsx
@@ -1,27 +1,41 @@
 import { Component, ReactNode } from "react";
 import Title from "./Title";
 import CustomLink from "./CustomLink";
+import { Helmet } from "react-helmet";
 
 export default class ProjectPage extends Component {
   render(): ReactNode {
     return (
-      <section className="ProjectPage">
-        <div className="container py-8 min-h-screen w-full h-full relative flex flex-col justify-center items-center">
-          <Title content="WoW" colorType="black" animation={false} />
-          <div className="mt-5 flex flex-col justify-center items-center text-xl">
-            <p className="text-center">
-              Your intrest in my story is amazing...
-            </p>
-            <p className="text-center">
-              ...However, I am still working on creating this section
-            </p>
-            <p className="text-center"> Stay tuned for the great launch!</p>
+      <>
+        <Helmet>
+          <title>Maintanance | Damian Trafiałek</title>
+          <meta
+            name="description"
+            content="Exciting Updates Underway! Our team is hard at work crafting a remarkable new feature for you. Stay tuned as we put the finishing touches on this addition to ensure a seamless experience. We can't wait to unveil it soon and enhance your journey with us. Thank you for your patience and support!"
+          />
+          <meta
+            name="keywords"
+            content="maintenance damian trafialek new feature coming up soon release"
+          />
+        </Helmet>
+        <section className="ProjectPage">
+          <div className="container py-8 min-h-screen w-full h-full relative flex flex-col justify-center items-center">
+            <Title content="WoW" colorType="black" animation={false} />
+            <div className="mt-5 flex flex-col justify-center items-center text-xl">
+              <p className="text-center">
+                Your intrest in my story is amazing...
+              </p>
+              <p className="text-center">
+                ...However, I am still working on creating this section
+              </p>
+              <p className="text-center"> Stay tuned for the great launch!</p>
+            </div>
+            <CustomLink to="/" className="p-8 my-10 flex items-center">
+              Back to exploring :)
+            </CustomLink>
           </div>
-          <CustomLink to="/" className="p-8 my-10 flex items-center">
-            Back to exploring :)
-          </CustomLink>
-        </div>
-      </section>
+        </section>
+      </>
     );
   }
 }
